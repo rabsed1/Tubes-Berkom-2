5b0bf3e0e0837e07d0e0e77089332e779d8b2628
Fachry Azriel <rabisunarddhy@gmail.com>
2025-12-09 22:18:22 -0800

    feat and fix: perbaikan algoritma dan penambahan attr

--- src/backend/config.py
+++ src/backend/config.py
@@ -2,10 +2,11 @@
 NAMA_BOXES = "boxes"
 NAMA_MEDIA = "media"
 NAMA_HISTORY = "history.json"
+
 JSON_INDENT = 4
 
 # KONFIG KARTU
-PANJANG_ATTR = 6
+ATTR_KARTU = 8
 
 IDX_DEPAN = 0
 IDX_BELAKANG = 1
@@ -13,7 +14,11 @@ IDX_NEXT = 2
 IDX_LAST = 3
 IDX_EF = 4 # Ini E-Factornya algo SM-2
 IDX_NR = 5 # Ini Number of Repetition, buat algo SM-2 juga
+IDX_Q_LAST = 6 # Ini Grade/quality jawaban, buat algo SM-2 juga
+IDX_R = 7
 
 # KONFIG SM-2
-MAX_NR_CLAMP = 20
-
+SM_MAX_NR = 20
+SM_DEFAULT_EF = 1.3
+SM_DEFAULT_NR = 0
+SM_DEFAULT_Q_LAST = 0
--- src/backend/scheduler.py
+++ src/backend/scheduler.py
@@ -15,7 +15,7 @@ def clamp(angka, maks):
 
 def jadwal_update(kartu, q, tanggal_today_str):
     # ALGORITMA SM-2
-    # Modifikasi sedikit: q di [0,4]. q < 2 dianggap incorrect.
+    # Modifikasi sedikit: q di [0,3]. q < 2 dianggap incorrect.
     # Versi original: q di [0,5]. q < 3 dianggap incorrect.
     # Alhasil formula EF juga berubah
 
@@ -24,7 +24,7 @@ def jadwal_update(kartu, q, tanggal_today_str):
     Ip = 0 # Ini interval (dalam hari)
 
     # Klasifikasi grade
-    if q >= 3:
+    if q >= 2:
         if NRp == 0:
             Ip = 1
         elif NRp == 1:
@@ -38,7 +38,7 @@ def jadwal_update(kartu, q, tanggal_today_str):
         Ip = 0 # Alias ngulang
     
     # Update nilai EF. Harusnya sebelum klasifikasi grade q sih
-    EFp = EFp + (0.1-(4-q)) * (.08+(4-q)*.02)
+    EFp = EFp + (0.1-(3-q)) * (.08+(3-q)*.02)
     if EFp < 1.3:
         EFp = 1.3
     
@@ -46,6 +46,7 @@ def jadwal_update(kartu, q, tanggal_today_str):
     kartu[config.IDX_NEXT] = tanggal_tambah(tanggal_today_str, Ip)
     kartu[config.IDX_EF] = EFp
     kartu[config.IDX_NR] = NRp
+    kartu[config.IDX_Q_LAST] = q
 
     return kartu
 
